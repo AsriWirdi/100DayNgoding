0404a1d9b1c517e668d9e7f2cddd39b3065a4b1e
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2024-09-05 09:44:07 +0800

    Add files via upload

---
+++ Day4 - System.out.print();/src/com/Mainn.java
@@ -0,0 +1,7 @@
+package com;
+
+public class Mainn {
+    public static void main(String[] args) {
+        System.out.print("hallow semuaa");
+    }
+}
---
+++ Day5 - komentar/src/com/Main.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Main {
+    public static void main(String[] args) {
+        // INI ADALAH KOMENTAR SATU BARIS
+        /*
+         * INI
+         * ADALAH
+         * KOMENTAR
+         * BANYAK
+         * BARIS
+         */
+        System.out.println("dd");
+    }
+}
